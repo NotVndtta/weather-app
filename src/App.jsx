8cf1d235e383e60eeffb6ca53f711f774076e2d8
NotVndtta <thenotalexey192@gmail.com>
2024-03-06 21:32:28 +0300

    Rebuilding project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import Navbar from "./components/Navbar";
-import Search from "./components/Search";
-import Temperature from "./components/Temperature";
-import TimeAndLocation from "./components/TimeAndLocation";
-
+import { useState } from "react"
+import './App.css'
+import search from './assets/icons/search.svg'
 export default function App() {
+
+  const [input,setInput] = useState('')
   return (
-    <div className="mx-auto max-w-screen-md nt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 
-    h-fit shadow-xl shadow-gray-400">
-      <Navbar />
-      <Search />
-      <TimeAndLocation />
-      <Temperature />
+    <div className="w-full h-screen text-white">
+      <nav className="w-full p-3 flex justify-between items-center">
+        <h1 className="font-bold tracking-wide text-3xl">Weather</h1>
+        <div className="bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2">
+        <input onKeyUp={(e) => {
+            if (e.key === 'Enter') {
+              // sumit the form
+              submitCity()
+            }
+          }} type="text" placeholder='Search city' className='focus:outline-none w-full text-[#212121] text-lg' 
+          value={input} 
+          onChange={e => setInput(e.target.value)} />
+        
+        </div>
+      </nav>
     </div>
   )
 }
